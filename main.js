c6670b7e3336261040f4da5b6aa019314fec6467
KAUANY BIANCA <o.kauany28@escola.pr.gov.br>
2026-06-30 09:42:03 -0300

    .

--- main.js
+++ main.js
@@ -16,6 +16,9 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2026-11-01");
+const tempoObjetivo2 = new Date("2027-11-01");
+const tempoObjetivo3 = new Date("2026-12-18");
+const tempoObjetivo4 = new Date("2150-11-01");
 
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
 
